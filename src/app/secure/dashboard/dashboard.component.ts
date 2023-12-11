48c82f4af3fa7c1bcb7e220984a3d29d97c214da
Yapsee <yapsasee@gmail.com>
2023-12-11 15:16:38 +0000

    improve dashboard

--- src/app/secure/dashboard/dashboard.component.ts
+++ src/app/secure/dashboard/dashboard.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CampaignService } from 'src/app/shared/services/campaign.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  campaigns: any;
 
+  constructor(
+    private campaignService: CampaignService
+  ) {
+
+  }
+
+  ngOnInit(): void {
+
+    this.campaignService.getAllCampaigns().subscribe(
+      (responseData: any) => {
+       this.campaigns = responseData
+      }
+    );
+  
+  }
+ 
 }
