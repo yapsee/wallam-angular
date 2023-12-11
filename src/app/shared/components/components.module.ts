a00847285365bc034613224303586bf7c6a45b4c
Yapsee <yapsasee@gmail.com>
2023-12-11 13:47:57 +0000

    add footer

--- src/app/shared/components/components.module.ts
+++ src/app/shared/components/components.module.ts
@@ -3,14 +3,15 @@ import { CommonModule } from '@angular/common';
 import { AuthcomponentComponent } from './authcomponent/authcomponent.component';
 import { CampaignsListComponent } from './campaigns-list/campaigns-list.component';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [AuthcomponentComponent, CampaignsListComponent,CampaignsListComponent],
+  declarations: [AuthcomponentComponent, CampaignsListComponent,CampaignsListComponent, FooterComponent],
   imports: [
     CommonModule, RouterModule
   ],
-  exports: [AuthcomponentComponent, CampaignsListComponent],
+  exports: [AuthcomponentComponent, CampaignsListComponent, FooterComponent],
 })
 export class ComponentsModule { }
